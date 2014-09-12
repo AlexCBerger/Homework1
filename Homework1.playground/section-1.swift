329de05b76e0a22fcaec04d267589f37a746e413
AlexCBerger <Alex@mwc-225090.dhcp.missouri.edu>
2014-09-11 19:50:04 -0500

    First commit

---
+++ Homework1.playground/section-1.swift
@@ -0,0 +1,68 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+let viewRect = CGRect(x: 0.0, y: 0.0, width: 320.0, height: 480.0)
+let view = UIView(frame: viewRect)
+view.backgroundColor = UIColor.lightGrayColor()
+
+let greenView = UIView(frame: CGRectZero)
+greenView.setTranslatesAutoresizingMaskIntoConstraints(false)
+greenView.backgroundColor = UIColor.greenColor()
+
+let yellowView = UIView(frame: CGRectZero)
+yellowView.setTranslatesAutoresizingMaskIntoConstraints(false)
+yellowView.backgroundColor = UIColor.yellowColor()
+
+let blueView = UIView(frame: CGRectZero)
+blueView.setTranslatesAutoresizingMaskIntoConstraints(false)
+blueView.backgroundColor = UIColor.blueColor()
+
+let orangeView = UIView(frame: CGRectZero)
+orangeView.setTranslatesAutoresizingMaskIntoConstraints(false)
+orangeView.backgroundColor = UIColor.orangeColor()
+
+view.addSubview(greenView)
+greenView.addSubview(yellowView)
+greenView.addSubview(blueView)
+greenView.addSubview(orangeView)
+
+
+let greenViewTopConstraint = NSLayoutConstraint(item: greenView, attribute: .Top, relatedBy: .Equal, toItem: view, attribute: .Top, multiplier: 1.0, constant: 20.0)
+
+let greenViewRightConstraint = NSLayoutConstraint(item: greenView, attribute: .Right, relatedBy: .Equal, toItem: view, attribute: .Right, multiplier: 1.0, constant: -16.0)
+
+let greenViewLeftConstraint = NSLayoutConstraint(item: greenView, attribute: .Left, relatedBy: .Equal, toItem: view, attribute: .Left, multiplier: 1.0, constant: 16.0)
+
+let greenViewBottomConstraint = NSLayoutConstraint(item: greenView, attribute: .Bottom, relatedBy: .Equal, toItem: view, attribute: .Bottom, multiplier: 1.0, constant: -10.0)
+
+let yellowViewBottomConstraint = NSLayoutConstraint(item: yellowView, attribute: .Bottom, relatedBy: .Equal, toItem: greenView, attribute: .Bottom, multiplier: 1.0, constant: -10.0)
+
+let yellowViewRightConstraint = NSLayoutConstraint(item: yellowView, attribute: .Right, relatedBy: .Equal, toItem: greenView, attribute: .Right, multiplier: 1.0, constant: -10.0)
+
+let yellowViewLeftConstraint = NSLayoutConstraint(item: yellowView, attribute: .Left, relatedBy: .Equal, toItem: greenView, attribute: .Left, multiplier: 1.0, constant: 10.0)
+
+let yellowViewTopConstraint = NSLayoutConstraint(item: yellowView, attribute: .Top, relatedBy: .Equal, toItem: greenView, attribute: .CenterY, multiplier: 1.0, constant: 3.0)
+
+let orangeViewBottomConstraint = NSLayoutConstraint(item: orangeView, attribute: .Bottom, relatedBy: .Equal, toItem: greenView, attribute: .CenterY, multiplier: 1.0, constant: -3.0)
+
+let orangeViewRightConstraint = NSLayoutConstraint(item: orangeView, attribute: .Right, relatedBy: .Equal, toItem: greenView, attribute: .Right, multiplier: 1.0, constant: -10.0)
+
+let orangeViewLeftConstraint = NSLayoutConstraint(item: orangeView, attribute: .Left, relatedBy: .Equal, toItem: greenView, attribute: .CenterX, multiplier: 1.0, constant: 3.0)
+
+let orangeViewTopConstraint = NSLayoutConstraint(item: orangeView, attribute: .Top, relatedBy: .Equal, toItem: greenView, attribute: .Top, multiplier: 1.0, constant: 10.0)
+
+let blueViewTopConstraint = NSLayoutConstraint(item: blueView, attribute: .Top, relatedBy: .Equal, toItem: greenView, attribute: .Top, multiplier: 1.0, constant: 10.0)
+
+let blueViewBottomConstraint = NSLayoutConstraint(item: blueView, attribute: .Bottom, relatedBy: .Equal, toItem: greenView, attribute: .CenterY, multiplier: 1.0, constant: -3.0)
+
+let blueViewRightConstraint = NSLayoutConstraint(item: blueView, attribute: .Right, relatedBy: .Equal, toItem: orangeView, attribute: .Left, multiplier: 1.0, constant: -6.0)
+
+let blueViewLeftConstraint = NSLayoutConstraint(item: blueView, attribute: .Left, relatedBy: .Equal, toItem: greenView, attribute: .Left, multiplier: 1.0, constant: 10.0)
+
+
+var constraints = [greenViewTopConstraint, greenViewRightConstraint, greenViewLeftConstraint, greenViewBottomConstraint, yellowViewBottomConstraint, yellowViewRightConstraint, yellowViewLeftConstraint, yellowViewTopConstraint, orangeViewBottomConstraint, orangeViewLeftConstraint, orangeViewRightConstraint, orangeViewTopConstraint, blueViewBottomConstraint, blueViewLeftConstraint, blueViewRightConstraint, blueViewTopConstraint]
+
+view.addConstraints(constraints)
+view.layoutIfNeeded()
+view
